04d74cdad0dc60eb49748bf29a481bffe1b83663
mazzutto <adam_mazur@ymail.com>
2023-01-31 17:52:22 +0100

    przykładowe typy zmiennych

--- src/Demo.java
+++ src/Demo.java
@@ -1,5 +1,43 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Demo");
+
+        byte b;
+        short s;
+        int i;
+        long l;
+        l = 1000000000000000000L; // duże liczby, na koncu duże L  !!
+
+        int number5; //deklaracja zmiennej
+        number5 = 5; //inicjalizacja zmiennej
+
+        int number = 10; //inicjalizacja zmiennej + deklaracja zmiennej
+        System.out.println(number);
+        System.out.println(5);
+
+
+        float f; // mniejsze od double
+        double zmiennoprzecinkowa = 9;
+        double zmiennoprzecinkowa2 = 4;
+        System.out.println(zmiennoprzecinkowa);
+        System.out.println(zmiennoprzecinkowa / zmiennoprzecinkowa2);
+        System.out.println(10.5);
+
+
+        String jakisteks = "Jakis";
+        System.out.println(jakisteks);
+        System.out.println("tekst w javie");
+
+        char character = 'a';
+        System.out.println(character);
+        System.out.println(character);
+        System.out.println('d');
+
+        boolean prawda = true;
+        System.out.println(prawda);
+        System.out.println(true);
+
+        boolean falsz = false;
+        System.out.println(falsz);
+        System.out.println(false);
     }
 }
